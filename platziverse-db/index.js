5f61b2cea720a4682f7c86a40595170b755eef73
Franco Coloccini <franco+github@coloccini.com.ar>
2020-05-07 12:48:48 -0500

    Commit inicial, clase 19 'Cómo hacer Mocks y Stubs con Sinon'

---
+++ platziverse-db/index.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const setupDatabase = require('./lib/db')
+const setupAgentModel = require('./models/agent')
+const setupMetricModel = require('./models/metric')
+const defaults = require('defaults')
+
+module.exports = async function (config) {
+  config = defaults(config, {
+    dialect: 'sqlite',
+    pool: {
+      max: 10,
+      min: 0,
+      idle: 10000
+    },
+    query: {
+      raw: true
+    }
+  })
+  const sequelize = setupDatabase(config)
+  const AgentModel = setupAgentModel(config)
+  const MetricModel = setupMetricModel(config)
+
+  AgentModel.hasMany(MetricModel)
+  MetricModel.belongsTo(AgentModel)
+
+  await sequelize.authenticate()
+
+  // Si la configuracion tiene la propiedad de setup listo, sincronizar.
+  // Esto, borra la bd actual si existe y, crea una nueva bd limpia.
+  if (config.setup) {
+    await sequelize.sync({ force: true })
+  }
+
+  const Agent = {}
+  const Metric = {}
+
+  return {
+    Agent,
+    Metric
+  }
+}
