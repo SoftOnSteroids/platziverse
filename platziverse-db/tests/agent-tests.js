aef388b688ddc322f943ff97c1fb53fefdab7ea9
Franco Coloccini <franco+github@coloccini.com.ar>
2020-05-13 01:12:31 -0500

    implementacion de findById y pruebas en el servicio Agent - clase 21

--- platziverse-db/tests/agent-tests.js
+++ platziverse-db/tests/agent-tests.js
@@ -27,6 +27,15 @@ test.beforeEach(async () => {
     hasMany: sandbox.spy()
   }
 
+  //Model findById Stub
+  AgentStub.findById = sandbox.stub()
+  //Sinon, cuando yo llame la funcion findById con el argumento id,
+  //me vas a retornar una promesa (porque estamos trabajando con
+  //una función asíncrona) que cuando la resuelvan va a retornar 
+  //agentFixtures.byId(id)
+  //En resumen - Usted es una función falsa, que cuando yo la llame va a retornar "esto"
+  AgentStub.findById.withArgs(id).returns(Promise.resolve(agentFixtures.byId(id)))
+
   const setupDatabase = proxyquire('../', {
     './models/agent': () => AgentStub,
     './models/metric': () => MetricStub
@@ -52,6 +61,10 @@ test.serial('Setup', t => {
 
 test.serial('Agent#findById', async t => {
   let agent = await db.Agent.findById(id)
+
+  t.true(AgentStub.findById.called, 'findById should be called on model')
+  t.true(AgentStub.findById.calledOnce, 'findById should be called once')
+  t.true(AgentStub.findById.calledWith(id), 'findById should be called with specified id')
 
   t.deepEqual(agent, agentFixtures.byId(id), 'should be the same')
 })
